1ffef469ed28266f6fe689c353358944d0d31b4f
deliasTheo <t22052002>
2022-02-16 16:35:51 +0100

    Piplip 20 fois

---
+++ PokePlup/PokePlup/Model/MyPokemon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokePlup.Model
+{
+    public class MyPokemon
+    {
+        public string Nom { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public string Attaque1 { get; set; }
+        public string Attaque2 { get; set;}
+
+    }
+}
